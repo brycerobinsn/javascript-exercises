54e10414afb5da76fd4c13f0a7fdd0a4a621012c
brycerobinsn <bryce.robinson@gmail.com>
2023-01-15 13:23:22 -0800

    complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let backwards = ''
+    for (let i = str.length -1; i >= 0; i--){
+        backwards += str[i]
+    }
+    return backwards
 };
 
 // Do not edit below this line

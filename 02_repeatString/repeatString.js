0824c71cbeefb7b9a3e83851aea340606757dc8e
brycerobinsn <bryce.robinson@gmail.com>
2023-01-15 13:08:36 -0800

    complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    words = []
+    if(num < 0) {
+        return 'ERROR'
+    }
+    for (let i = 0; i < num; i++){
+        words.push(string)
+    }
+    return words.join('')
 };
 
 // Do not edit below this line

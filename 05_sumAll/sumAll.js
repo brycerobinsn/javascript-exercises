dfcfd84b22a81bd72623d36d9103388ead0552e7
brycerobinsn <bryce.robinson@gmail.com>
2023-01-15 14:20:15 -0800

    complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let total = 0
+    let start = 0
+
+    if (num1 < 0 || num2 < 0){
+        return 'ERROR'
+    } else if (Number.isInteger(num1 + num2)){
+        if (num1 > num2){
+            start = num2
+        }
+        else{
+            start = num1
+        }
+        for(let i = 0; i <= Math.abs(num2 - num1); i++){
+            let add = start + i
+            total += add
+        }
+        return total
+    }
+    return 'ERROR'
 
 };
 

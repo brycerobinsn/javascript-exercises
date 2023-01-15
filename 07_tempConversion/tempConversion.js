a81f87043d012f8e0763e6e1e251334313fbabc8
brycerobinsn <bryce.robinson@gmail.com>
2023-01-15 14:54:31 -0800

    complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const rounder = function(num){
+  return Math.round(num  * 10) / 10
+}
+const convertToCelsius = function(temp) {
+  // (X - 32) x 5/9
+  let y = (temp - 32) * (5/9)
+  return rounder(y)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  // (X x 9/5 + 32)
+  let y = temp * (9/5) + 32
+  return rounder(y)
 };
 
 // Do not edit below this line
